e6eba6f4de1130c5dcc34410b77d06c1ef3e39df
TripleLLL9 <86983398+TripleLLL9@users.noreply.github.com>
2023-04-13 17:11:39 -0400

    Create newDiscordGameSDK.js

---
+++ newDiscordGameSDK.js
@@ -0,0 +1,61 @@
+class discordGameSDK {
+
+    var rpc = null;
+    var clientId = 'YOUR_CLIENT_ID_HERE';
+    var accessToken = null;
+    var authenticated = false;
+
+    ext._shutdown = function() {
+        if (rpc && authenticated) {
+            rpc.clearActivity();
+            rpc.close();
+        }
+    };
+
+    ext._getStatus = function() {
+        if (rpc && authenticated) {
+            return {status: 2, msg: 'Connected'};
+        }
+        return {status: 1, msg: 'Not connected'};
+    };
+
+    ext.initialize = function() {
+        if (!rpc) {
+            DiscordRPC.register(clientId);
+            rpc = new DiscordRPC.Client({ transport: 'ipc' });
+            rpc.on('ready', () => {
+                console.log('Discord RPC ready!');
+                authenticated = true;
+            });
+            rpc.login({ clientId }).catch(console.error);
+        }
+    };
+
+    ext.setActivity = function(details, state, largeImageKey, smallImageKey) {
+        if (rpc && authenticated) {
+            rpc.setActivity({
+                details: details,
+                state: state,
+                largeImageKey: largeImageKey,
+                smallImageKey: smallImageKey,
+                instance: false
+            });
+        }
+    };
+
+    ext.clearActivity = function() {
+        if (rpc && authenticated) {
+            rpc.clearActivity();
+        }
+    };
+
+    var descriptor = {
+        blocks: [
+            [' ', 'Initialize Discord Game SDK', 'initialize'],
+            [' ', 'Set Discord Game SDK activity details: %s state: %s large image key: %s small image key: %s', 'setActivity', '', '', '', ''],
+            [' ', 'Clear Discord Game SDK activity', 'clearActivity'],
+        ]
+    };
+
+    ScratchExtensions.register('Discord Game SDK', descriptor, ext);
+})({});
