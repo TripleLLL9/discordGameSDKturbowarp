11fd658bfa8b92a597e0401eb8ec09c3f2b630a7
TripleLLL9 <86983398+TripleLLL9@users.noreply.github.com>
2023-04-23 09:58:07 -0400

    Update networkExample.js

--- networkExample.js
+++ networkExample.js
@@ -11,18 +11,29 @@ class NetworkExample {
           arguments: {
             URL: {
               type: Scratch.ArgumentType.STRING,
-              defaultValue: 'https://example.com',
+              defaultValue: 'https://jsonplaceholder.typicode.com/todos/1'
             }
           }
         },
         {
-          opcode: 'get_element_by_id',
+          opcode: 'getElementById',
           blockType: Scratch.BlockType.REPORTER,
-          text: 'get element by ID [ID]',
+          text: 'get element by id [ID]',
           arguments: {
             ID: {
               type: Scratch.ArgumentType.STRING,
-              defaultValue: 'element-id',
+              defaultValue: 'example-id'
+            }
+          }
+        },
+        {
+          opcode: 'fragmentGet',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'fragment.get [SELECTOR]',
+          arguments: {
+            SELECTOR: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'p'
             }
           }
         }
@@ -31,19 +42,25 @@ class NetworkExample {
   }
 
   fetch(args) {
-    return fetch(args.URL)
-      .then(response => response.text())
-      .then(text => text.slice(0, 100)); // Truncate to first 100 characters
+    const url = args.URL;
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => JSON.stringify(json));
   }
-  
-  get_element_by_id(args) {
+
+  getElementById(args) {
     const id = args.ID;
     const element = document.getElementById(id);
     if (element) {
-      return element.outerHTML;
-    } else {
-      return '';
+      return element.innerText;
     }
+    return '';
+  }
+
+  fragmentGet(args) {
+    const selector = args.SELECTOR;
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    return fragment.get(selector);
   }
 }
 
